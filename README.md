10494c02368ea9f4c5a8d9665ffb0fb9efd28e73
Ari Mirsky <arimirsky@gmail.com>
2024-08-21 16:07:50 -0400

    Add automatic db_location determination

---
+++ README.md
@@ -0,0 +1,3 @@
+# Usage
+1. Open “Privacy & Security” item in System Preferences. Click on "Full Disk Access". Add Terminal.
+2. From the root directory, run main.py, calling all wanted functions.
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # pylint: skip-file
 import imessage_reader
-import contact_reader
 import datetime
+import os
 
-DB_LOCATION = '/Users/arimirsky/Documents/iMessageReader/chat.db'
+DB_LOCATION = f'{os.path.expanduser("~")}/Library/Messages/chat.db'
 
 imessage_reader.all_data(DB_LOCATION, smooth_hours=24*30)
